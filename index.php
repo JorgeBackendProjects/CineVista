164c111d5451f5d513b72b99466cebc12e55aa89
Jorge-DAM-Ceuta <jorgemugar12@hotmail.com>
2024-05-05 21:50:36 +0200

    Revisar forma de recorrer actores

--- index.php
+++ index.php
@@ -1,109 +1,103 @@
 <?php
 
-// Obtener todos los actores del reparto de una película. 
-// JUNTAR LAS DOS FUNCIONES Y GUARDAR EN LA PELICULA UN ARRAY DE ID DE ACTORES...
-function get_casting($movie_id) {
-    $casting_data = array();
-
-    $get_cast_movie = curl_init();
-
-    curl_setopt($get_cast_movie, CURLOPT_URL, "https://api.themoviedb.org/3/movie/" . $movie_id . "/credits?language=es-ES&api_key=107cc8a9703efd86f41232ea75b85039");
-    curl_setopt($get_cast_movie, CURLOPT_RETURNTRANSFER, true); 
-
-    // Recojo la película correspondiente al id con que se ha hecho la solicitud.
-    $casting = json_decode(curl_exec($get_cast_movie));
-
-    // Cerrar sesión cURL.
-    curl_close($get_cast_movie);
-
-    array_push($casting_data, $casting);
-
-    // Devolvemos el id de cada personal de reparto de la película.
-    return $casting_data;
-}
-
-$reparto = get_casting(11);
-var_dump($reparto);
-
-function get_actors_casting($reparto) {
-    $actors = array();
-
-    // Devolvemos el id de cada personal de reparto de la película.
-    //return $casting_data;
-
-    foreach ($reparto as $actor) {
-        $get_actor = curl_init();
-
-        curl_setopt($get_actor, CURLOPT_URL, "https://api.themoviedb.org/3/person/" . $actor->id . "?language=es-ES&api_key=107cc8a9703efd86f41232ea75b85039");
-        curl_setopt($get_actor, CURLOPT_RETURNTRANSFER, true); 
-
-        // Recojo la película correspondiente al id con que se ha hecho la solicitud.
-        $respuesta = json_decode(curl_exec($get_actor));
-
-        // Cerrar sesión cURL.
-        curl_close($get_actor);
-
-        // Guardamos el actor en el array.
-        $info_actor = array(
-            "id" => $respuesta->id,
-            "nombre" => $respuesta->name,
-            "personaje" => $actor->character,
-            "biografia" => $respuesta->biography,
-            "lugar_nacimiento" => $respuesta->place_of_birth,
-            "bithday" => $respuesta->birthday,
-            "deathday" => $respuesta->deathday,
-            "genero" => $respuesta->gender == 1 ? "Femenino" : ($respuesta->gender == 2 ? "Masculino" : "Otro"),
-            "departamento" => $respuesta->known_for_department,
-            "popularidad" => $respuesta->popularity,
-            "imagen" => $respuesta->profile_path,
-            "peliculas" => $respuesta->also_known_as
-        );
-
-        //array_push($actors, $casting);
-    }
-}
-
-/*function get_popular_movies() {
+function get_popular_movies() {
     $movies_data = array();
+    $actors_data = array();
 
-    for ($i = 1; $i <= 5; $i++) {
+    for ($i = 1; $i <= 2; $i++) {
         // Inicializa la sesión cURL para la solicitud.
         $get_popular_movies = curl_init();
-
-        // Solicitud a la api para recoger los resultados de la primera página... ¿Quizá tenga que hacer la solicitud en un bucle e iterar la página?
+        // Solicitud a la api para recoger los resultados de la página.
         curl_setopt($get_popular_movies, CURLOPT_URL, "https://api.themoviedb.org/3/discover/movie?page=" . $i . "&language=es-ES&sort_by=popularity.desc&api_key=107cc8a9703efd86f41232ea75b85039");
         // Hacemos que la respuesta se guarde en una variable.
         curl_setopt($get_popular_movies, CURLOPT_RETURNTRANSFER, true); 
-
         // Ejecutar la solicitud y guardar la respuesta
         $popular_movies = json_decode(curl_exec($get_popular_movies))->results;
-
         // Cerrar sesión cURL
         curl_close($get_popular_movies);
 
-        // Recorremos las películas de la página...
+        // Recorremos cada película de la página.
         foreach ($popular_movies as $pelicula) {
+            $casting_data = array();
+            $id_actors = array();
+
         // PELICULA
             $get_one_movie = curl_init();
-
             curl_setopt($get_one_movie, CURLOPT_URL, "https://api.themoviedb.org/3/movie/" . $pelicula->id . "?language=es-ES&api_key=107cc8a9703efd86f41232ea75b85039");
             curl_setopt($get_one_movie, CURLOPT_RETURNTRANSFER, true); 
-
             // Recojo la película correspondiente al id con que se ha hecho la solicitud.
             $movie = json_decode(curl_exec($get_one_movie));
-
-            // Cerrar sesión cURL.
             curl_close($get_one_movie);
+        
+        // REPARTO
+            $get_cast_movie = curl_init();
+            curl_setopt($get_cast_movie, CURLOPT_URL, "https://api.themoviedb.org/3/movie/" . $pelicula->id . "/credits?language=es-ES&api_key=107cc8a9703efd86f41232ea75b85039");
+            curl_setopt($get_cast_movie, CURLOPT_RETURNTRANSFER, true); 
+            // Recojo el casting de la pelicula y lo guardo en el array.
+            $casting = json_decode(curl_exec($get_cast_movie));
+            array_push($casting_data, $casting);
+            curl_close($get_cast_movie);
+        
+            //var_dump($casting_data[0]->cast);
+
+        // ACTORES
+            // Por cada actor del casting de la película guardo sus datos.
+            $contador_actor = 0;
+            foreach ($casting_data[0]->cast as $actor) {
+                $id = isset($actor->id) ? $actor->id : null;
+                $lugar_nacimiento = isset($actor->place_of_birth) ? $actor->place_of_birth : null;
+                $nombre = isset($actor->name) ? $actor->name : null;
+
+                $get_actor = curl_init();
+                curl_setopt($get_actor, CURLOPT_URL, "https://api.themoviedb.org/3/person/" . $actor->id . "?language=es-ES&api_key=107cc8a9703efd86f41232ea75b85039");
+                curl_setopt($get_actor, CURLOPT_RETURNTRANSFER, true); 
+                // Recojo la película correspondiente al id con que se ha hecho la solicitud.
+                $actor_info = json_decode(curl_exec($get_actor));
+                // Cerrar sesión cURL.
+                curl_close($get_actor);
+
+                if (!isset($actor_info->success)) {
+                    if ($actor_info->known_for_department == "Acting") {
+                        $actor_data = array(
+                            "id" => $id,
+                            "nombre" => $nombre,
+                            "personaje" => isset($actor_info->character) ? $actor_info->character : null,
+                            "biografia" => isset($actor_info->biography) ? $actor_info->biography : null,
+                            "lugar_nacimiento" => isset($lugar_nacimiento) ? $lugar_nacimiento : null,
+                            "bithday" => $lugar_nacimiento,
+                            "deathday" => isset($actor_info->deathday) ? $actor_info->deathday : null,
+                            "genero" => isset($actor_info->gender) ? ($actor_info->gender == 1 ? "Femenino" : ($actor_info->gender == 2 ? "Masculino" : "Otro")) : null,
+                            "departamento" => isset($actor_info->known_for_department) ? $actor_info->known_for_department : null,
+                            "popularidad" => isset($actor_info->popularity) ? $actor_info->popularity : null,
+                            "imagen" => isset($actor_info->profile_path) ? $actor_info->profile_path : null,
+                            "peliculas" => isset($actor_info->also_known_as) ? $actor_info->also_known_as : null
+                        );
+
+                        var_dump($actor_data);
+                    }
+                }
+
+                
+
+                
+
+                /*if (array_filter($actor_data, 'strlen')) {
+                    array_push($actors_data, $actor_data);
+                    array_push($id_actors, $actor_data["id"]);
+                }*/
+            }
+
+            //var_dump($id_actors);
 
             // Guardamos la película en el array. // Añadir subarray con los id de los actores obtenidos en cada registro del array de reparto.
             $movie_data = array(
+                "id" => $movie->id, // Es el id por el que he buscado en la petición.
                 "adult" => $movie->adult, // Boolean, si es de adultos o no.
                 "backdrop_path" => $movie->backdrop_path, // Fondo de la película que seguramente no haga falta.
                 "belongs_to_collection" => $movie->belongs_to_collection, // Me devuelve 4 datos que seguramente no hagan falta.
                 "budget" => $movie->budget, // Me devuelve el presupuesto.
                 "genres" => $movie->genres, // Es un array que puede tener varios géneros. Recorrer y por cada uno obtener el género con la key ["name"].
                 "homepage" => $movie->homepage, // Página donde ver la película.
-                "id" => $movie->id, // Es el id por el que he buscado en la petición.
                 "imdb_id" => $movie->imdb_id, // Id que seguramente no haga falta.
                 "origin_country" => $movie->origin_country, // Array (un solo valor?) con String.
                 "original_language" => $movie->original_language, // Valor en/es etc... Hacer función para modificarlo y guardarlo sin abreviatura.
@@ -123,19 +117,20 @@ function get_actors_casting($reparto) {
                 "video" => $movie->video, // Boolean, si hay o no vídeo.
                 "vote_average" => $movie->vote_average, // Float con puntuación. 
                 "vote_count" => $movie->vote_count, // Int con total de calificaciones obtenidos.
+                "id_actors" => $id_actors // ID de todos los actores del reparto.
             );
             
             array_push($movies_data, $movie_data);
         }
     }
 
-    return array("movies" => $movies_data);
+    return array("movies" => $movies_data, "actors" => $actors_data);
 }
 
-//$resultado = get_popular_movies();
+$resultado = get_popular_movies();
 //var_dump($resultado);
 
-*/
+
 
 
 
